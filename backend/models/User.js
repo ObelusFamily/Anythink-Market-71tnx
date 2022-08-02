4e1e22408bab9904fe94ced4fc14c2960c2de224
sethu400 <sethupriya076@gmail.com>
2022-08-02 18:11:34 +0530

    Added items

--- backend/models/User.js
+++ backend/models/User.js
@@ -74,7 +74,7 @@ UserSchema.methods.toAuthJSON = function() {
     email: this.email,
     token: this.generateJWT(),
     bio: this.bio,
-    image: this.image,
+    image: this.image || "placeholder.png",
     role: this.role
   };
 };
